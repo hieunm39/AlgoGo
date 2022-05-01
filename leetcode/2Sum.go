402c089451ab78b5b9e9f7ceb2162581fb07847c
Hieu Nguyen <hieungming@gmail.com>
2022-05-01 13:49:49 +0700

    2Sum problem

--- leetcode/2Sum.go
+++ leetcode/2Sum.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-
 func main() {
 
 	result := twoSum([]int{1,2,3,4,5}, 7);
@@ -19,7 +18,6 @@ func twoSum(nums []int, target int) []int {
 
 	for i := 0; i < len(nums) - 1; i++ {
 		if _, prs := hashMap[nums[i]]; prs {
-		
 			return []int{ hashMap[nums[i]], i};
 		}
 
