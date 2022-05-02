73f5446da55e849933ccecbb4dbfebbab64f8347
Hieu Nguyen <hieungming@gmail.com>
2022-05-02 13:16:24 +0700

    containsDuplicate problem

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
-import (
-	"net/http"
-)
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-}
 
 func main() {
 
 	
-
-	http.HandleFunc("/hello", hello);
-
-	http.ListenAndServe(":8090", nil);
 	
 }	
 
